0e5ed10891a91b1510434222424f6a1ae11d392c
LukeTurp <luketurpin7@gmail.com>
2018-04-24 16:16:31 -0700

    #5 Login Completed
    Set up paths for build when rendering from Flask.  Set up script source when rendering from flask in index.html.  Wired up login through Redux.  Completed Reducers and Actions.

--- application/app.py
+++ application/app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app=Flask(__name__, static_folder='application/templates/src')
+app=Flask(__name__, static_folder='./static/dist', template_folder='./static/public')
 
 @app.route('/', methods=['GET'])
 def index():
-    return send_from_directory('./templates/public', 'index.html')
+    return render_template('index.html')
+
+@app.route('/<path:path>', methods=['GET'])
+def all_root_paths(path):
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
--- application/static/src/modules/forms.js
+++ application/static/src/modules/forms.js
@@ -1,26 +1,53 @@
+import axios from 'axios'
+
 export const LOGIN = 'forms/LOGIN'
+export const LOGIN_REQUESTED = 'forms/LOGIN_REQUESTED'
+export const LOGIN_SUCCESS = 'forms/LOGIN_SUCCESS'
+export const LOGIN_FAILED = 'forms/LOGIN_FAILED'
 
 
 const initialState = {
-    credentials: {}
+    loginAttempting: false,
+    username: undefined,
+    access_token: undefined,
+    refresh_token: undefined
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case LOGIN:
+        case LOGIN_REQUESTED:
             return {
                 ...state,
-                credentials: state
+                username: action.payload,
+                loginAttempting: true
             }
-        default: 
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                loginAttempting: !state.loginAttempting,
+                access_token: action.payload.data.access_token,
+                refresh_token: action.payload.data.refresh_token
+            }
+        case LOGIN_FAILED:
+            return {
+                ...state,
+                error: action.error
+            }
+        default:
             return state
     }
 }
 
-export const login = () => {
+export const login = credentials => {
     return dispatch => {
-        dispatch({
-            type: LOGIN
-        })
+      debugger;
+        dispatch({ type: LOGIN_REQUESTED, payload: credentials.username })
+
+          const request = axios.post('http://127.0.0.1:8000/api/v1/user/auth', credentials)
+
+        request.then(
+                resp => dispatch({ type: LOGIN_SUCCESS, payload: resp }),
+                err => dispatch({ type: LOGIN_FAILED, error: err })
+            )
     }
 }
